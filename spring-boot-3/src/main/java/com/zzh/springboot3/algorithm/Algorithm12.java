0326839218ebfb4be3db24f39cffaddd99b8918d
zzh <171832195@qq.com>
2024-09-13 16:19:57 +0800

    二分法

--- spring-boot-3/src/main/java/com/zzh/springboot3/algorithm/Algorithm12.java
+++ spring-boot-3/src/main/java/com/zzh/springboot3/algorithm/Algorithm12.java
@@ -3185,6 +3185,48 @@ public class Algorithm12 {
 
         return max;
     }
+
+
+    /**
+     * 35. 搜索插入位置
+     */
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (target <= nums[0]) {
+            return 0;
+        }
+        if (target >= nums[nums.length - 1]) {
+            return target == nums[nums.length - 1] ? nums.length - 1 : nums.length;
+        }
+        return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+
+    public int binarySearch(int[] nums, int left, int right, int target) {
+        if (left > right) {
+            return left;
+        }
+        int mid = (right + left) / 2;
+        if (target == nums[mid]) {
+            return mid;
+        }
+        if (target > nums[mid]) {
+            return binarySearch(nums, mid + 1, right, target);
+        } else {
+            return binarySearch(nums, left, mid - 1, target);
+        }
+    }
+
+    @Test
+    public void testSearchInsert() {
+        int[] nums = {1, 3};
+        searchInsert(nums, 3);
+    }
+
+
+    
 }
 
 
