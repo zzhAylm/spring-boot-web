ad98f134a4bae0f5cbab39053dcb577fdb9a6685
zzh <171832195@qq.com>
2024-09-14 15:10:12 +0800

    动态规划

--- spring-boot-3/src/main/java/com/zzh/springboot3/algorithm/Algorithm12.java
+++ spring-boot-3/src/main/java/com/zzh/springboot3/algorithm/Algorithm12.java
@@ -3226,7 +3226,22 @@ public class Algorithm12 {
     }
 
 
-    
+    /**
+     * 70. 爬楼梯
+     */
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+        int[] nums = new int[n + 1];
+        nums[1] = 1;
+        nums[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            nums[i] = nums[i - 1] + nums[i - 2];
+        }
+        return nums[n];
+    }
+
 }
 
 
